db3b4f875f178026b9cb83c2fb1456c356355e0b
gassuncao <gassuncao@symplicity.com>
2025-10-15 23:41:10 -0300

    feat: jwt auth with testes and solid principles

--- app_backend/routes/api.php
+++ app_backend/routes/api.php
@@ -6,7 +6,11 @@ use App\Http\Controllers\Auth\AuthController;
 Route::prefix('auth')->controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('login', 'login');
-    Route::post('refresh', 'refresh');
-    Route::post('logout', 'logout');
-    Route::get('me', 'me');
+
+    Route::middleware('auth:api')->group(function () {
+        Route::get('me', 'me');
+        Route::post('logout', 'logout');
+        Route::post('refresh', 'refresh');
+    });
 });
+
